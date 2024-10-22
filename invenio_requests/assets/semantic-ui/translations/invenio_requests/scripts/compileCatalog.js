3c53bdaa7dd30d90072cbd9b7ac6b8e1962016d9
David Eckhard <dabombeeer@gmail.com>
2024-10-22 16:38:32 +0200

    i18n: Update commands for message extraction and compilation
    * Since Transifex is used to manage translations, all we have
      to provide is a .pot file as a source. This will be pushed
      to Transifex.
    * English is the default language, so this is generated on extraction.
    * When pulling from Transifex, .po files will be generated/updated.
    * On package release, all available language files (.po) are  compiled
      into .json files. These are then used by the application for lookup.

--- invenio_requests/assets/semantic-ui/translations/invenio_requests/scripts/compileCatalog.js
+++ invenio_requests/assets/semantic-ui/translations/invenio_requests/scripts/compileCatalog.js
@@ -1,14 +1,13 @@
 // This file is part of React-Invenio-Deposit
-// Copyright (C) 2021 Graz University of Technology.
+// Copyright (C) 2021-2024 Graz University of Technology.
 //
 // Invenio-app-rdm is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 
-const { readFileSync, writeFileSync } = require("fs");
+const { appendFileSync, readdirSync, readFileSync, writeFileSync } = require("fs");
 const { gettextToI18next } = require("i18next-conv");
 
-const PACKAGE_JSON_BASE_PATH = "./";
-const { languages } = require(`../package`).config;
+const PACKAGE_MESSAGES_PATH = "./messages";
 
 // it accepts the same options as the cli.
 // https://github.com/i18next/i18next-gettext-converter#options
@@ -22,19 +21,58 @@ function save(target) {
   };
 }
 
-if ("lang" === process.argv[2]) {
-  const lang = process.argv[3];
+function compileAndCreateFileForLanguage(parentPath, lang) {
   gettextToI18next(
     lang,
-    readFileSync(`${PACKAGE_JSON_BASE_PATH}messages/${lang}/messages.po`),
+    readFileSync(`${parentPath}/${lang}/messages.po`),
     options
-  ).then(save(`${PACKAGE_JSON_BASE_PATH}messages/${lang}/translations.json`));
-} else {
+  ).then(save(`${parentPath}/${lang}/translations.json`));
+}
+
+function writeGeneratedTranslationsFile(languages) {
+  const generatedTranslationsFilePath = `${PACKAGE_MESSAGES_PATH}/_generatedTranslations.js`;
+  writeFileSync(
+    generatedTranslationsFilePath,
+    "// This is an auto generated file to import and export all available translations.\n" +
+      "// Changes should not be checked into version control.\n"
+  );
+
   for (const lang of languages) {
-    gettextToI18next(
-      lang,
-      readFileSync(`${PACKAGE_JSON_BASE_PATH}messages/${lang}/messages.po`),
-      options
-    ).then(save(`${PACKAGE_JSON_BASE_PATH}messages/${lang}/translations.json`));
+    appendFileSync(
+      generatedTranslationsFilePath,
+      `import TRANSLATE_${lang.toUpperCase()} from "./${lang}/translations.json";\n`
+    );
   }
+
+  appendFileSync(generatedTranslationsFilePath, `\nexport const translations = {\n`);
+  for (const lang of languages) {
+    appendFileSync(
+      generatedTranslationsFilePath,
+      `  ${lang}: { translation: TRANSLATE_${lang} },\n`
+    );
+  }
+  appendFileSync(generatedTranslationsFilePath, `};\n`);
+}
+
+if ("lang" === process.argv[2]) {
+  const lang = process.argv[3];
+  compileAndCreateFileForLanguage(`${PACKAGE_MESSAGES_PATH}`, lang);
+} else {
+  // Since we use Transifex for managing translations, the pulled .po files have to
+  // be converted to .json files in order to be used by the application.
+  const directories = readdirSync(`${PACKAGE_MESSAGES_PATH}`, {
+    withFileTypes: true,
+  }).filter((dir) => dir.isDirectory());
+
+  // We now assume we are dealing with directories containing a messages.po file
+  // - read input file containing translations (e.g. de.po)
+  // - write compiled output file containing translations to be used by the application (e.g. de.json)
+  let languages = [];
+  for (const directory of directories) {
+    compileAndCreateFileForLanguage(directory.parentPath, directory.name);
+    languages.push(directory.name);
+  }
+
+  // - write file containing translations for static import in i18next.js
+  writeGeneratedTranslationsFile(languages);
 }
